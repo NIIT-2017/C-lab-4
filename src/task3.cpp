1176869743186b2879317202c9ab436a70d4668f
DmitriKonnovNN <44975416+DmitriKonnovNN@users.noreply.github.com>
2019-03-04 02:23:23 +0300

    Add files via upload

--- src/task3.cpp
+++ src/task3.cpp
@@ -6,8 +6,9 @@
 
 int isPalindrome(char * str)
 {
+	char * p_Left = str;
 	char * p_Right = str + strlen(str) - 1; // Warum laesst sich die Variable als die des Zyklus initialisieren?
-	for (char * p_Left = str; p_Left < p_Right; p_Left++, p_Right--)
+	for (p_Left; p_Left < p_Right; p_Left++, p_Right--)
 		{
 			if (*p_Left != *p_Right)
 				return 0;
