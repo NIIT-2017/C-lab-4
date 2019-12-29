9cfa6386083dc6366b6aad3f62b91ce5bbcd0be3
Koeco97 <komissarovaolesya@yandex.ru>
2019-12-29 17:08:57 +0300

    renamed fp

--- src/main4.cpp
+++ src/main4.cpp
@@ -8,33 +8,35 @@
 int main()
 {
     char* strBegin[strl] = { NULL };
-    FILE* fp1;
-    fp1 = fopen("wisdom.txt", "rt");
-    if (!fp1)
+    FILE* fp;
+    fp = fopen("wisdom.txt", "rt");
+    if (!fp)
     {
         puts("Error!");
         return 1;
     }
-    FILE* fp2;
-    fp2 = fopen("wisdom2.txt", "wt");
-    if (!fp2)
-    {
-        puts("Error!");
-        return 1;
-    }
+    
     char str[strl][symbl];
     int ch = 0;
     int size = 0;
-    while (fgets(str[size], symbl, fp1) && size < strl)
+    while (fgets(str[size], symbl, fp) && size < strl)
     {
         strBegin[size] = str[size];
         size++;
     }
-    /*   for (int i = 0; i < size; i++)
-           printf("%s", str[i]);*/
+	fclose(fp);
     lineSort(strBegin, size);
-    printLinesToFile((const char**)strBegin, size, fp2);
-    fcloseall();
+	
+	FILE* fp;
+    fp = fopen("wisdom2.txt", "wt");
+    if (!fp)
+    {
+        puts("Error!");
+        return 1;
+    }
+	
+    printLinesToFile((const char**)strBegin, size, fp);
+	fclose(fp);
     return 0;
 }
 
