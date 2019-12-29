a2bae4799e4b9fd92e3c41f72ad1e57c671ca0db
Olesya <57029712+Koeco97@users.noreply.github.com>
2019-12-29 17:32:01 +0300

    Update main4.cpp

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,29 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
+#include <stdio.h>
 #define strl 164
 #define symbl 512
 #include "task4.h"
 
 int main()
 {
+    char str[strl][symbl];
     char* strBegin[strl] = { NULL };
     FILE* fp;
+	
     fp = fopen("wisdom.txt", "rt");
     if (!fp)
     {
         puts("Error!");
         return 1;
     }
-    
-    char str[strl][symbl];
-    int ch = 0;
+
     int size = 0;
     while (fgets(str[size], symbl, fp) && size < strl)
     {
         strBegin[size] = str[size];
         size++;
     }
-	fclose(fp);
+    fclose(fp);
+	
     lineSort(strBegin, size);
 	
     fp = fopen("wisdom2.txt", "wt");
