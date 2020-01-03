fc48ff9df5cd163f8c5119331e4ad7e4d2db1487
Evgeny <57017613+ekaryagin@users.noreply.github.com>
2020-01-03 21:47:15 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -4,17 +4,15 @@
 void lineSort(char* str[], int size)
 {
 	char* bubble = NULL;
-	for (int k = 0; k < size; k++)
-	{
-		for (int i = 0; i < size; i++)
+	
+	for (int k = 0; k < size-1; k++)
+		for (int i = 0; i < size-1-k; i++)
 			if (strlen(str[i]) > strlen(str[i + 1]))
 			{
 				bubble = str[i];
 				str[i] = str[i + 1];
 				str[i + 1] = bubble;
 			}
-		//size--;
-	}
 }
 
 void printLines(const char* str[], int size)
