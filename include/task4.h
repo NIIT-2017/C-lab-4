8ba836eb9f3804b6b491cd61b7b4d16ac288083f
MarinaSpasskova <nerra@yandex.ru>
2020-01-10 08:56:01 +0100

    Trying to fix task4

--- include/task4.h
+++ include/task4.h
@@ -1,1 +1,3 @@
+#include <stdio.h>
+
 void printLinesToFile(const char *str[],int size,FILE *fp);
--- src/task4.cpp
+++ src/task4.cpp
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-
-void printLinesToFile(const char *str[],int size,FILE *fp) {
+void printLinesToFile(const char *str[],int size, FILE *fp) {
     int i = 0;
     for (i = 0; i<size; i++) {
         fprintf(fp, "%s\n", str[i]);
