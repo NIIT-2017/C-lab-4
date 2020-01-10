3d139c819c140eb9280aa126c4cbc785a929c0ac
MarinaSpasskova <nerra@yandex.ru>
2020-01-10 08:43:25 +0100

    Add missed code

--- src/task2.cpp
+++ src/task2.cpp
@@ -2,22 +2,23 @@
 #include <stdlib.h>
 # include <string.h>
 
-char* reverseWords(char* in, char* out) {
-    char* arr[128];
+char *reverseWords(char *in, char *out) {
+    char *arr[128];
     int i = 0;
     int j = 0;
-    char* pWord;
+    char *pWord;
     int inWord = 0;
-    char* firstElment = out;
+    char *firstElment = out;
 
-    while (*in) {                           
+    while (*in) {
         if (*in != ' ' && inWord == 0) {     //You read a string up to the end and looking for first word in string
             arr[i] = in;                    //You write down an adress of first word`s symbol of wotd in array of pointers
             i++;
+            inWord = 1;
+        } else if (*in == ' ' && inWord == 1) {
+            inWord = 0;
         }
-        else if (*in == ' ' && inWord == 1) {
-            inWord;
-        }  in++;                                    //You move to next element in  array of pointers
+        in++;                                    //You move to next element in  array of pointers
     }
 
     for (j = i - 1; j >= 0; j--) {                //You read a string from the end to begining
@@ -27,6 +28,10 @@ char* reverseWords(char* in, char* out) {
             pWord++;
             out++;
         }
+        *out = ' ';
+        out++;
     }
+    out--;
+    *out = 0;
     return firstElment;
 }
