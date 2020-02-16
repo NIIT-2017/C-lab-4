51e0f3446b2c37df8d9bebb9b93c5584031712ae
SvetlanaKov <lanaiks2404@gmail.com>
2020-02-16 17:31:03 +0300

    Changed

---
+++ src/main2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "task2.h"
+
+
+int main()
+{
+    char in[SIZE] = { 0 };
+    char out[SIZE] = { 0 };
+    
+    printf("Enter a string: ");
+    fgets(in, SIZE, stdin);
+    if (in[strlen(in - 1)] == '\n')
+    {
+        in[strlen(in - 1)] = 0;
+    }
+   printf("Reversed string: %s", reverseWords(in, out));
+    return 0;
+}
---
+++ src/main4.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "task1.h"
+#include "task4.h"
+
+int main()
+{
+	char str[SIZE][128] = { {0} };
+	char * pstr[SIZE] = { 0 };
+	int size = 0;
+
+	FILE * fp;
+	fp = fopen("wisdomR.txt", "rt");
+	if (!fp) 
+	{
+		puts("File error!");
+		return 1;
+	}
+	while (fgets(str[size], 128, fp)) 
+	{
+		if (str[size][strlen(str[size]) - 1] == '\n') 
+		{
+			str[size][strlen(str[size]) - 1] = 0;
+		}
+		pstr[size] = str[size];
+		size++;
+	}
+
+	lineSort(pstr, size);
+	
+	FILE * fp1;
+	fp1 = fopen("wisdomW.txt", "w");
+	if (!fp1) 
+	{
+		puts("File error!");
+		return 1;
+	}
+	printLinesToFile((const char**)pstr, size, fp1);
+	fcloseall();
+	
+	return 0;
+}
