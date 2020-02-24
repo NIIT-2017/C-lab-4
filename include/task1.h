565f7d515067b6d01322b8982f7874313299452c
IFedyanin <57591664+IFedyanin@users.noreply.github.com>
2020-02-24 15:00:33 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -1,6 +1,6 @@
-void lineSort(char* str[], int size);
-void printLines(const char* str[], int size);
 #include <stdio.h>
 #include <string.h>
 #define SYMBOLS_IN_LINE 256
 #define LINES 256 
+void lineSort(char* str[], int size);
+void printLines(const char* str[], int size);
