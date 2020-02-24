1a0ccd130a3570c92bdd9d63553196df2269d1b0
IFedyanin <57591664+IFedyanin@users.noreply.github.com>
2020-02-24 15:22:08 +0300

    Update task1.cpp

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,43 +1,26 @@
-#include "task1.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
-int max_line(char* str[], int size, int i)
+int comp(const void *a, const void *b)
 {
-    int min_strlen = size;
-
-    while (i < size)
-    {
-        if (strlen(str[i]) < min_strlen)
-            min_strlen = strlen(str[i]);
-
-        i++;
-    }
-
-    return min_strlen;
+	return strlen(*(char**)a) - strlen(*(char**)b);
 }
 
-void lineSort(char* str[], int size)
+void lineSort(char *str[], int size)
 {
-    int i = 0;
-    int j = 0;
-    char* p;
-    while (i < size)
-    {
-        if (strlen(str[i]) == max_line(str, size, j))
-        {
-            p = str[j];
-            str[j] = str[i];
-            str[i] = p;
+	qsort(str, size, sizeof(char**), comp);
 
-            j++;
-            i = j;
-        }
-        else
-            i++;
-    }
+
 }
 
-void printLines(const char* str[], int size)
+void printLines(const char *str[], int size)
 {
-    for (int i = 0; i < size; i++)
-        printf("%s\n", str[i]);
-}
+	for (int i = 0; i < size; i++)
+	{
+		puts(str[i]);
+	}
+
+
+} 
