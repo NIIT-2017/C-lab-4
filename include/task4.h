147941dee58e4992ab5e5f92a4061813c80a82e0
IFedyanin <57591664+IFedyanin@users.noreply.github.com>
2020-02-24 15:02:00 +0300

    Update task4.h

--- include/task4.h
+++ include/task4.h
@@ -1,9 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-
-void lineSort(char* str[], int size);
-void printLinesToFile(const char* str[], int size, FILE* fp);
-
 #define LINES 256
 #define SYMBOLS_IN_LINE 256
+void lineSort(char* str[], int size);
+void printLinesToFile(const char* str[], int size, FILE* fp);
