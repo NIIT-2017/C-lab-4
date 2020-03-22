5a3623d81ea535e3ab42a2fc5b562037b2b9bbc4
Natalya Grigoreva <n-tixom@mail.ru>
2020-03-22 21:51:54 +0300

    lab4v3

--- src/task4.cpp
+++ src/task4.cpp
@@ -1,22 +1,7 @@
 #include "task4.h"
 #include <string.h>
 
-/*void lineSort(char *str[],int size)
-{
-	
-	for (int i=0; i<size-1; i++)
-		for (int j=(size-1); j>i; j--)
-		{
-			if (strlen(str[j-1])>strlen(str[j]))
-			{
-				char *buf=str[j-1];
-				str[j-1]=str[j];
-				str[j]=buf;
-			}
 
-		}
-}
-*/
 
 void printLinesToFile(const char *str[], int size, FILE *fp)
 {
