cdf4486cdce45f4968086b71bb246c727f699746
Rusbrus <57061175+Rusbrus@users.noreply.github.com>
2020-03-27 20:58:46 +0300

    Create main2.cpp

---
+++ src/main2.cpp
@@ -0,0 +1,22 @@
+#include "task2.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define SIZE 256
+
+int main()
+{
+	char in[SIZE];
+	char out[SIZE];
+	
+	printf("Input the line\n");
+	fgets(in, 256, stdin);
+
+	if (in[strlen(in) - 1] == '\n')
+	{
+		in[strlen(in) - 1] = 0;
+	}
+
+	printf("%s", reverseWords(in, out));
+	return 0;
+}
